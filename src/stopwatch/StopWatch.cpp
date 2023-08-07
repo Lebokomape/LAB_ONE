c4f5f223b008f0163bad48d919b597528f19edf2
marvenkomape <marvenkom272@gmail.com>
2023-08-07 18:08:30 +0200

    Exercise 5.1

--- src/stopwatch/StopWatch.cpp
+++ src/stopwatch/StopWatch.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 using namespace std;
 
+//Exercise 5.1
 // returns the amount of time in seconds that has passed since the process (i.e. your program) started executing
 double getProcessTime()
 {
